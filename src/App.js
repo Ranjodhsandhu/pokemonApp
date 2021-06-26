6cfd66d559f0b03f60c7eaee62fb06250341f972
Ranjodh Singh <ranjodhsingh24@gmail.com>
2021-06-26 15:42:57 -0400

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
 import './App.css';
+import Header from './Header';
+import PokemonCard from './PokemonCard';
+
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [pokemon,setPokemon] = useState([]);
+
+  useEffect(()=>{
+    async function getPokemon(){
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const data = await response.json();
+      
+      setPokemon(data.results);
+    }
+    getPokemon();
+  },[]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header message="Pokemon App" />
+      <Container className={classes.cardGrid}>
+        <Grid container justify="center" spacing={6} alignItems="center">
+          {pokemon.map((poke)=>{
+            return (
+              <Grid item key={poke.name}>
+                <PokemonCard url={poke.url}/>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/PokemonCard.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState}from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+export default function PokemonCard({url}) {
+    const classes = useStyles();
+    const [name,setName] = useState('');
+    const [imageUrl,setImageUrl] = useState('');
+    useEffect(()=>{
+        async function getPokemon(){
+            const response = await fetch(url);
+            const data = await response.json();
+
+            setName(data.name);
+            setImageUrl(data.sprites.other['official-artwork'].front_default);
+        }
+        getPokemon();
+    }, []);
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    component="img"
+                    image={imageUrl}
+                    title={`Image of the pokemon ${name}`}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
